acfa50b87db73d03d6c745160ed04be99b8b5f8f
Carekowski <phgp1111@gmail.com>
2026-07-30 15:19:32 -0300

    a

--- src/control/fuzzy/funcreg.cpp
+++ src/control/fuzzy/funcreg.cpp
@@ -5,9 +5,11 @@ EZER = Triangular       VZER = Triangular
 EPQP = Triangular       VPQP = Triangular
 EGRP = Trapezoidal      VGRP = Trapezoidal
 */
-#include funcreg.h
-#include func.h
+#include <funcreg.h>
+#include <func.h>
 
+void fuzzy::funcoes(float erro, float varerro)
+{
 EGRN = trapmf(erro,-3500,-3500,-2000,-1500); //analisar na calibragem do sensor se os valores estao corretos
 EPQN = trimf(erro,-1750,-1125,-500);
 EZER = trimf(erro,-700,0,700);
@@ -19,3 +21,6 @@ VPQN = trimf(varerro,-1750,-1125,-500);
 VZER = trimf(varerro,-700,0,700);
 VPQP = trimf(varerro,500,1125,1750);
 VGRP = trapmf(varerro,1500,2000,3500,3500);
+}
+
+//Regras (25 regras)
--- src/control/fuzzy/funcreg.h
+++ src/control/fuzzy/funcreg.h
@@ -7,6 +7,8 @@ class funcreg
 public:
 
 private:
+float EGRN, EPQN, EZER, EPQP, EGRP;
+float VGRN, VPQN, VZER, VPQP, VGRP;
 }
 
 struct RegrasSaida
